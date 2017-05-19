67b65441eeb64a41c14946be282c711a8e51321c
Nándor Urszuly <ursznandor@users.noreply.github.com>
2017-05-19 16:33:56 +0200

    added html structure

---
+++ calculator.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+
+<HTML>
+<HEAD>
+	<meta charset="utf-8">
+	<title>Small Calculator in PHP</title>
+</HEAD>
+<BODY>
+	<div class="calc">
+		<p>Calculator</p>
+	</div>
+	<form>
+		<input type="text" name="number1" placeholder="Number 1">
+		<input type="text" name="number2" placeholder="Number 2">
+		<select name="operator">
+			<option>add</option>
+			<option>subtrack</option>
+			<option>multiply</option>
+			<option>divide</option>
+		</select><br>
+		<button type="submit" name="submit" value="submit">CALCULATE</button>
+	</form>
+</BODY>
+</HTML>
