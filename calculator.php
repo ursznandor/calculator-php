da56b9aa8d018ee34a06573f6d2280f06d86baa0
Nándor Urszuly <ursznandor@users.noreply.github.com>
2017-05-20 10:26:41 +0200

    added core.php

--- calculator.php
+++ calculator.php
@@ -13,6 +13,7 @@
 		<input type="text" name="number1" placeholder="Number 1">
 		<input type="text" name="number2" placeholder="Number 2">
 		<select name="operator">
+			<option>none</option>
 			<option>add</option>
 			<option>subtrack</option>
 			<option>multiply</option>
